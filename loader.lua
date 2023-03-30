b0478fdd9a180af62bea8ac61fca97a0fe70ef5e
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-03-30 13:57:50 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -3,10 +3,10 @@ local TM = 12422074717
 local GameID = game.PlaceId
 
 if GameID == BFS then
-    loadstring(game:HttpGet("https://github.com/Patooh777/WickedHub/blob/Main/Smash%26Grab.lua?raw=true", true))()
+    loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/BoxingFightersSimulator.lua?raw=true", true))()
     
 elseif GameID == TM then
-    loadstring(game:HttpGet("https://github.com/Patooh777/WickedHub/blob/Main/CarTycoon.lua?raw=true", true))()
+    loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/TappingMasters.lua?raw=true", true))()
     
 else
     print("invalid Place")
