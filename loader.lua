4ae12c68d022a4d5c8a6b61a48f32f6645310dbd
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-03-31 17:58:14 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,7 @@
 local BFS = 12289293395
 local TM = 12422074717
 local FH = 11162791099
+local AS = 10521031051
 local GameID = game.PlaceId
 
 if GameID == BFS then
@@ -10,7 +11,8 @@ elseif GameID == TM then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/TappingMasters.lua?raw=true", true))()
 elseif GameID == FH then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/FreeHatchers.lua?raw=true", true))()
-    
+elseif GameID == AS
+    loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/AnimeSimulator.lua?raw=true", true))()
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
