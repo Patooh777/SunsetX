d2fde7de358317326ab49d272dcd771e7b921cde
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-04-09 00:35:20 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,6 +2,7 @@ local BFS = 12289293395
 local TM = 12422074717
 local FH = 11162791099
 local AS = 10521031051
+local PM = 10202329527
 local GameID = game.PlaceId
 
 if GameID == BFS then
@@ -13,6 +14,8 @@ elseif GameID == FH then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/FreeHatchers.lua?raw=true", true))()
 elseif GameID == AS then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/AnimeSimulator.lua?raw=true", true))()
+elseif GameID == PM then
+    loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/ProjectMugetsu.lua?raw=true", true))()
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
