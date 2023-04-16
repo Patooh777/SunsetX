837562812dff084dc01b9e83148a29cbc0df799d
Patooh <53504041+Patooh777@users.noreply.github.com>
2023-04-16 14:24:06 -0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,7 +15,6 @@ elseif GameID == AS then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/AnimeSimulator.lua?raw=true", true))()
 elseif GameID == ALS then
     loadstring(game:HttpGet("https://github.com/Patooh777/SunsetX/blob/Main/Games/AnimeLostSimulator.lua?raw=true", true))()
-    end
 else
     print("invalid Place")
     game.Players.LocalPlayer:Kick("Ur script dont support this shit ass game")
